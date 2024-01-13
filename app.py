9d1e58ac580c09bc1be35ac57cb0713d3e8172ff
MohsenRazavi <mohsenrazavi.self@gmail.com>
2024-01-13 23:58:38 +0330

    complete admin dashboard

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ def login():
 def admin_panel():
     return render_template('./admin_main_page.html')
 
+@app.route('/employee')
+def employee_panel():
+    return render_template('./employee_main_page.html')
+
+@app.route('/user')
+def user_panel():
+    return render_template('./user_main_page.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
