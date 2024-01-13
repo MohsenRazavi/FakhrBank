8a0854accda74d1ee58b5ba30b1b8c704da3e01a
MohsenRazavi <mohsenrazavi.self@gmail.com>
2024-01-14 00:14:53 +0330

    implemented dashboaed for user and employee

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ def login():
 
 @app.route('/admin')
 def admin_panel():
-    return render_template('./admin_main_page.html')
+    return render_template('./admin_dashboard.html')
 
 @app.route('/employee')
 def employee_panel():
-    return render_template('./employee_main_page.html')
+    return render_template('./employee_dashboard.html')
 
 @app.route('/user')
 def user_panel():
-    return render_template('./user_main_page.html')
+    return render_template('./user_dashboard.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
